5f6a5a4bd1a91f342f417346be64293e04471de1
Pooya Emami <pooyaemamivar77@gmail.com>
2026-07-25 13:53:18 +0330

    fixed memcached script

--- 02/master/memcached_init_master.sh
+++ 02/master/memcached_init_master.sh
@@ -5,7 +5,6 @@ echo "[Master] Initializing Memcached..."
 echo "IP: $MEMCACHED_IP"
 echo "PORT: $MEMCACHED_PORT"
 
-# Install if missing
 if ! command -v memcached &> /dev/null; then
     echo "[Master] Memcached not found. Installing..."
     sudo apt update
@@ -14,16 +13,21 @@ else
     echo "[Master] Memcached already installed."
 fi
 
-# Stop systemd's memcached so it doesn't fight over the port
 sudo systemctl stop memcached 2>/dev/null
-
-# Kill any manually-started instance (exact match, won't kill this script)
 pkill -x memcached 2>/dev/null
 sleep 1
 
 echo "[Master] Starting memcached manually..."
 memcached -l "$MEMCACHED_IP" -p "$MEMCACHED_PORT" -d
-sleep 2
+
+echo "[Master] Waiting for memcached to be ready..."
+for i in {1..20}; do
+    if echo -n "" | nc -w1 "$MEMCACHED_IP" "$MEMCACHED_PORT" 2>/dev/null; then
+        echo "[Master] Memcached is up."
+        break
+    fi
+    sleep 0.5
+done
 
 echo "[Master] Testing memcached..."
 printf "set testkey 0 60 4\r\ntest\r\nget testkey\r\nquit\r\n" \
