041fca24ccea53e249b14e2d406614a88761eaaa
Pooya Emami <pooyaemamivar77@gmail.com>
2026-07-25 12:28:25 +0330

    part 2 updated

--- 02/scripts/build_and_run.sh
+++ 02/scripts/build_and_run.sh
@@ -9,13 +9,23 @@ fi
 
 cd ../$ROLE || exit 1
 
+# 1. Initialize SQLite DB
+if [ "$ROLE" = "master" ]; then
+    ./db_init_master.sh
+else
+    ./db_init_slave.sh
+fi
+
+# 2. Initialize Memcached
 if [ "$ROLE" = "master" ]; then
     ./memcached_init_master.sh
 else
     ./memcached_init_slave.sh
 fi
 
+# 3. Build
 make clean
 make
 
+# 4. Run server
 ./${ROLE}_server "$CONFIG"
